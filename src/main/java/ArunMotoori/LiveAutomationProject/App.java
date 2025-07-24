22b018ec1a484ac3cc4b75c8486984c58bd8486f
Sharanya N R <sharanyanagoorravindra@gmail.com>
2025-07-24 12:17:29 +0530

    TC_RF_001

---
+++ src/main/java/ArunMotoori/LiveAutomationProject/App.java
@@ -0,0 +1,13 @@
+package ArunMotoori.LiveAutomationProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
